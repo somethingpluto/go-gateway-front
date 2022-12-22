bda03c4e730eb992890fdfd1cb6b85604ac08b17
HaoXuan chen <1575197604@qq.com>
2022-12-22 20:01:11 +0800

    fix

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">网关后台管理</h3>
+        <h3 class="title">Go网关后台管理</h3>
       </div>
 
       <el-form-item prop="username">
