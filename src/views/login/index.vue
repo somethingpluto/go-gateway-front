cd90c17b0209bbe59fedb9a6ded50274dc555013
HaoXuan chen <1575197604@qq.com>
2023-01-02 19:10:58 +0800

    fix:ui change

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Go网关后台管理</h3>
+        <h1 class="title">Go网关后台管理</h1>
       </div>
 
       <el-form-item prop="username">
@@ -17,7 +17,7 @@
           name="username"
           type="text"
           tabindex="1"
-          auto-complete="on"
+          auto-complete="off"
         />
       </el-form-item>
 
@@ -33,7 +33,6 @@
           placeholder="Password"
           name="password"
           tabindex="2"
-          auto-complete="on"
           @keyup.enter.native="handleLogin"
         />
         <span class="show-pwd" @click="showPwd">
@@ -127,33 +126,21 @@ $bg:#283443;
 $light_gray:#fff;
 $cursor: #fff;
 
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
 /* reset element-ui css */
 .login-container {
   .el-input {
     display: inline-block;
     height: 47px;
     width: 85%;
-
+    font-size: 20px;
     input {
       background: transparent;
       border: 0px;
       -webkit-appearance: none;
-      border-radius: 0px;
+      border-radius: 10px;
       padding: 12px 5px 12px 15px;
       color: $light_gray;
       height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
     }
   }
 
@@ -172,10 +159,16 @@ $dark_gray:#889aa4;
 $light_gray:#eee;
 
 .login-container {
-  min-height: 100%;
+  // min-height: 100%;
+  // width: 100%;
+  // background-color: $bg;
+  // overflow: hidden;
   width: 100%;
-  background-color: $bg;
-  overflow: hidden;
+  height: 100%;
+  background-image: url("../../assets/images/background.png");
+  background-size:cover;
+  background-position:center;
+  position: relative;
 
   .login-form {
     position: relative;
@@ -210,11 +203,14 @@ $light_gray:#eee;
     position: relative;
 
     .title {
-      font-size: 26px;
-      color: $light_gray;
+      font-size: 36px;
+      color: white;
       margin: 0px auto 40px auto;
       text-align: center;
       font-weight: bold;
+      background-color: #66b1ff;
+      padding: 10px;
+      border-radius: 20px;
     }
   }
 
